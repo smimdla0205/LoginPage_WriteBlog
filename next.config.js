7809591c020df2e665bbfe0a3f3f63cb8762ae82
limSangMin <sangmin0205@gmail.com>
2024-07-10 18:16:04 +0900

    초기세팅중

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
+
+        return config;
+    },
+};
